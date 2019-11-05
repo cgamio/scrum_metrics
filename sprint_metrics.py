a89ec1208ba5a6ef304728817992172a84e5d445
Chris Gamio <chris.gamio@hearst.com>
2019-11-05 14:05:37 -0500

    Fix for divide by zero and url encoding issues

--- sprint_metrics.py
+++ sprint_metrics.py
@@ -10,7 +10,8 @@ import urllib.parse
 jira_host = 'thetower.atlassian.net'
 jira_url = f"https://{jira_host}/rest/agile/1.0/"
 greenhopper_url = f"https://{jira_host}/rest/greenhopper/1.0/"
-jira_query_url = f"https://{jira_host}/issues/?jql=issueKey in ("
+jira_query_url = f"https://{jira_host}/issues/?jql="
+jira_query_jql = "issueKey in ("
 
 # Auth Data
 netrc = netrc.netrc()
@@ -245,8 +246,10 @@ def getAvgVelocity(board_id, sprintID):
         elif str(velocityEntryKey) == str(sprintID):
             threeSprintVelocityTotal = velocityEntries[velocityEntryKey]['completed']['value']
             sprintCounter = sprintCounter + 1
-
-    return threeSprintVelocityTotal/sprintCounter
+    if sprintCounter > 0:
+        return threeSprintVelocityTotal/sprintCounter
+    else:
+        return 0
 
 
 def getListOfIssueIdsStr(listOfIssues):
@@ -275,9 +278,9 @@ def getNotionSection(sprint_data, boardID, sprint_report):
             "Predictability of Commitments "+str('{:6.2f}'.format(points['planned_completed']/points['committed']*100))+"%",
             "Average Velocity (Three Sprints) "+str('{:6.2f}'.format(avgVelocity)),
             "Bugs "+str(items['bugs_completed']),
-            "Completed Issues URL: " + jira_query_url + completedIssuesIds + ")",
-            "Not Completed Issues URL: " + jira_query_url + notCompletedIssuesIds + ")",
-            "Removed Issues URL: " + jira_query_url + removedIssuesIds + ")"
+            "Completed Issues URL: " + jira_query_url +  urllib.parse.quote(jira_query_jql + completedIssuesIds + ")"),
+            "Not Completed Issues URL: " + jira_query_url+ urllib.parse.quote(jira_query_jql + notCompletedIssuesIds + ")"),
+            "Removed Issues URL: " + jira_query_url+ urllib.parse.quote(jira_query_jql + removedIssuesIds + ")")
     ]
 
 def collectSprintData(projectKey, sprintID=False):
