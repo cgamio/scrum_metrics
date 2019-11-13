09aed59dfd6c865c027eed9febe60127605d42f5
Chris Gamio <chris.gamio@hearst.com>
2019-11-13 16:23:59 -0500

    New NotionPage object with rudimetary createSprintReport function

---
+++ notion_page.py
@@ -0,0 +1,25 @@
+import os
+
+from notion.client import NotionClient
+from notion.block import PageBlock, HeaderBlock
+from sprint_metrics import *
+from datetime import datetime
+
+class NotionPage:
+
+    def __init__(self, url):
+        self.__client = NotionClient(token_v2=os.environ.get('NOTION_TOKEN'))
+        self.blocks = self.__client.get_block(url)
+
+    def createSprintReport(self, sprint_data):
+        sprint_report = self.blocks.children.add_new(PageBlock, title=f"{sprint_data['project_name']} Sprint {sprint_data['sprint_number']} Report")
+
+        start_date = datetime.strptime(sprint_data['sprint_start'].split('T')[0], '%Y-%m-%d')
+        end_date = datetime.strptime(sprint_data['sprint_end'].split('T')[0], '%Y-%m-%d')
+        date_string = datetime.strftime(start_date, '%m/%d/%Y')
+        date_string += " - "
+        date_string += datetime.strftime(end_date, '%m/%d/%Y')
+
+        sprint_report.children.add_new(HeaderBlock, title=f"[{sprint_data['project_name']} Sprint {sprint_data['sprint_number']} Report ({date_string})]('https://www.google.com')")
+
+        return sprint_report
